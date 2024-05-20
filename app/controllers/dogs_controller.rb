438de378e359d8bd11545fe414f3b83d2a8721c2
George Richmond <georgerichmond@gmail.com>
2024-05-20 09:59:52 +0100

    Random breed image works, handling basic edge cases

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,33 @@
 class DogsController < ApplicationController
+  require 'net/http'
+
+  def fetch_dog_image
+    breed = params[:breed].strip.downcase
+
+    if breed.blank?
+      @breed = params[:breed]
+      @image_url = nil
+      @message = "Breed name cannot be empty. Please enter a valid breed."
+    else
+      url = URI("https://dog.ceo/api/breed/#{breed}/images/random")
+      response = Net::HTTP.get(url)
+      data = JSON.parse(response)
+
+      Rails.logger.debug "API Response: #{data}"
+
+      @breed = params[:breed]
+      if data['status'] == 'success'
+        @image_url = data['message']
+        @message = "Here's your pic for #{@breed}"
+      else
+        @image_url = nil
+        @message = "#{@breed} not found. Perhaps try a different breed, like 'Corgi'"
+      end
+    end
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to root_path }
+    end
+  end
 end
--- test/system/dogs_test.rb
+++ test/system/dogs_test.rb
@@ -2,10 +2,67 @@ require "application_system_test_case"
 
 class DogsTest < ApplicationSystemTestCase
   # Happy Path Tests
-  test "visiting the index" do
+  test "visiting the index and submitting form with 'hound'" do
     visit root_path
 
     assert_selector "h1", text: "Dog Breed Pics"
 
+    fill_in "Breed", with: "hound"
+    click_on "Submit"
+
+    assert_text "Here's your pic for hound"
+    assert_selector "img"
   end
+
+  test "visiting the index and submitting form with 'Corgi'" do
+    visit root_path
+
+    fill_in "Breed", with: "Corgi"
+    click_on "Submit"
+
+    assert_text "Here's your pic for Corgi"
+    assert_selector "img"
+  end
+
+  # Unhappy Path Tests
+  test "visiting the index and submitting form with 'Unicorn'" do
+    visit root_path
+
+    fill_in "Breed", with: "Unicorn"
+    click_on "Submit"
+
+    assert_text "Unicorn not found. Perhaps try a different breed, like 'Corgi'"
+    assert_no_selector "img"
+  end
+
+  test "visiting the index and submitting form with empty breed" do
+    visit root_path
+
+    click_on "Submit"
+
+    assert_text "Breed name cannot be empty. Please enter a valid breed."
+    assert_no_selector "img"
+  end
+
+  # Additional Tests
+  test "visiting the index and submitting form with case insensitive breed 'HOUND'" do
+    visit root_path
+
+    fill_in "Breed", with: "HOUND"
+    click_on "Submit"
+
+    assert_text "Here's your pic for HOUND"
+    assert_selector "img"
+  end
+
+  test "visiting the index and submitting form with leading and trailing whitespace '  hound  '" do
+    visit root_path
+
+    fill_in "Breed", with: "  hound  "
+    click_on "Submit"
+
+    assert_text "Here's your pic for hound"
+    assert_selector "img"
+  end
+
 end
